a355d24b2365668898bed6c0daf51fabb46ef2a9
Meg Stepp <mcstepp@users.noreply.github.com>
2019-04-21 23:17:25 -0400

    fix icon bug

--- app/Http/Controllers/GraphicsController.php
+++ app/Http/Controllers/GraphicsController.php
@@ -21,7 +21,7 @@ class GraphicsController extends Controller
      */
     public function update(Request $request, IGraphics $owner)
     {
-        $this->authorize('update', Graphics::class, $owner);
+        $this->authorize('update', $owner);
 
         $validated = $request->validate([
            'avatar' => ['nullable','url','regex:/https:\/\/([a-z0-9]+(?:[a-z0-9-]*[a-z0-9])?[.])*nickpic[.]host\/?(.*)/'],
