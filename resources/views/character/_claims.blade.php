cf2d310f8144d7e4c9ba68d0c4f26829848551cf
Meg Stepp <mcstepp@users.noreply.github.com>
2019-05-06 23:02:47 -0400

    update details descriptions

--- resources/views/character/_claims.blade.php
+++ resources/views/character/_claims.blade.php
@@ -1,7 +1,13 @@
 <div class="col-xl-4">
     <div class="card m-card bg-industrial-dark">
         <div class="card-body">
-            Occupation
+            <h4 class="text-uppercase m-fancy-title d-inline-block">Industry | </h4>
+            <span>The specific category of jobs that your character works. Every faction specializes in a specific set of industries. If your character is an Initiate, their industry is Initiation.</span>
+        </div>
+
+        <div class="card-body">
+            <h4 class="text-uppercase m-fancy-title d-inline-block">Job | </h4>
+            <span>The specific role your character has within their industry. Initiate is considered a job.</span>
         </div>
     </div>
 </div>
