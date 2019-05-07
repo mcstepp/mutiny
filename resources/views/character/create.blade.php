206465873a35450373ae6a67c676e06025efb057
Meg Stepp <mcstepp@users.noreply.github.com>
2019-05-06 23:16:55 -0400

    update wip descriptions

--- resources/views/character/create.blade.php
+++ resources/views/character/create.blade.php
@@ -47,36 +47,46 @@
                         @include('character._claims')
                     </div>
 
-                    <div class="row">
-                        <div class="card m-card bg-industrial-dark">
-                            <div class="card-top">
-                                <h3 class="m-fancy-title uppercase text-center">
-                                    Submission
-                                </h3>
-                                <hr class="glow-default">
-                            </div>
-                            <div class="card-body">
-                                <h4 class="text-uppercase m-fancy-title d-inline-block">Work In Progress | </h4>
-                                <span>
-                            Your character's given name at birth. Your character might go by a nickname, or a shortened version of their name, or may have even given themselves a new name at initiation. There's a different section for that after this field. What is the first name on the birth certificate of your character?
-                        </span>
-                            </div>
-                        </div>
-
-                        <div class="card m-card">
-                            <div class="card-body">
-                                <div class="form-group row px-5">
-                                    <div class="form-check text-center my-0 mx-auto">
-                                        <input class="form-check-input" type="checkbox" name="save" id="wip" checked>
-                                        <label class="form-check-label" for="wip">Work In Progress</label>
-                                    </div>
+                    <div class="row pt-4">
+                        <div class="container-fluid mb-5">
+                            <div class="row">
+                                <div class="col-md-6 order-2 order-md-first">
+                                    <hr class="glow-default d-flex">
+                                </div>
+                                <div class="col-md-6 order-1 order-md-last">
+                                    <h2 class="m-fancy-header neon-default text-uppercase text-center">
+                                        Submission
+                                    </h2>
                                 </div>
                             </div>
-
                         </div>
 
+                        <div class="col-xl-4">
+                            <div class="card m-card bg-industrial-dark">
+                                <div class="card-body">
+                                    <h4 class="text-uppercase m-fancy-title d-inline-block">Work In Progress | </h4>
+                                    <span>Not finished yet? Save your progress for later. You will be able to see your pending character's profile, but it will not be added to the queue for acceptance unless this is <b>unchecked</b>.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-8">
+                            <div class="card m-card">
+                                <div class="card-body">
+                                    <div class="form-group row px-5">
+                                        <div class="form-check text-center my-0 mx-auto">
+                                            <input class="form-check-input form-control form-control-lg" type="checkbox" name="save" id="wip" checked>
+                                            <label class="form-check-label" for="wip">Work In Progress</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
                         <input type="hidden" name="owner" value="{{ $character->user_id }}">
-                        <button type="submit" class="btn btn-primary text-uppercase m-fancy-title">Submit Character</button>
+                        <button type="submit" class="btn btn-primary text-uppercase m-fancy-title mx-auto">Submit Character</button>
                     </div>
                 </div>
             </form>
--- resources/views/pending_characters/edit.blade.php
+++ resources/views/pending_characters/edit.blade.php
@@ -8,8 +8,7 @@
             <h1 class="h2 page-title m-fancy-header neon-default">Edit a New Character</h1>
         </div>
         <div class="page-desc p-md-5 m-md-2">
-            <p>
-                It's time to create a character! Fill out the application below and within 24-48 hours, you will receive acceptance or feedback from a staff member. If you have any questions, feel free to send a private message to `disco before the breakdown` or post in the Welcome forum!
+            <p>Make your edits or additions to the application below and within 24-48 hours, you will receive acceptance or feedback from a staff member. If you have any questions, feel free to send a private message to `disco before the breakdown` or post in the <a href="#linkToWelcomeForum">Welcome forum</a>!
             </p>
         </div>
 
@@ -49,36 +48,46 @@
                         @include('character._claims')
                     </div>
 
-                    <div class="row">
-
-                        <div class="card m-card bg-industrial-dark">
-                            <div class="card-top">
-                                <h3 class="m-fancy-title uppercase text-center">
-                                    Submission
-                                </h3>
-                                <hr class="glow-default">
-                            </div>
-                            <div class="card-body">
-                                <h4 class="text-uppercase m-fancy-title d-inline-block">Work In Progress | </h4>
-                                <span>
-                                    Your character's given name at birth. Your character might go by a nickname, or a shortened version of their name, or may have even given themselves a new name at initiation. There's a different section for that after this field. What is the first name on the birth certificate of your character?
-                                </span>
-                            </div>
-                        </div>
-
-                        <div class="card m-card">
-                            <div class="card-body">
-                                <div class="form-group row px-5">
-                                    <div class="form-check text-center my-0 mx-auto">
-                                        <input class="form-check-input" type="checkbox" name="save" id="wip" checked>
-                                        <label class="form-check-label" for="wip">Work In Progress</label>
-                                    </div>
+                    <div class="row pt-4">
+                        <div class="container-fluid mb-5">
+                            <div class="row">
+                                <div class="col-md-6 order-2 order-md-first">
+                                    <hr class="glow-default d-flex">
+                                </div>
+                                <div class="col-md-6 order-1 order-md-last">
+                                    <h2 class="m-fancy-header neon-default text-uppercase text-center">
+                                        Submission
+                                    </h2>
                                 </div>
                             </div>
-
                         </div>
 
-                        <button type="submit" class="btn btn-primary text-uppercase m-fancy-title">Save Character</button>
+                        <div class="col-xl-4">
+                            <div class="card m-card bg-industrial-dark">
+                                <div class="card-body">
+                                    <h4 class="text-uppercase m-fancy-title d-inline-block">Work In Progress | </h4>
+                                    <span>Not finished yet? Save your progress for later. You will be able to see your pending character's profile, but it will not be added to the queue for acceptance unless this is <b>unchecked</b>.</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-8">
+                            <div class="card m-card">
+                                <div class="card-body">
+                                    <div class="form-group row px-5">
+                                        <div class="form-check text-center my-0 mx-auto">
+                                            <input class="form-check-input form-control form-control-lg" type="checkbox" name="save" id="wip" checked>
+                                            <label class="form-check-label" for="wip">Work In Progress</label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                        <input type="hidden" name="owner" value="{{ $character->user_id }}">
+                        <button type="submit" class="btn btn-primary text-uppercase m-fancy-title mx-auto">Submit Character</button>
                     </div>
                 </div>
             </form>
