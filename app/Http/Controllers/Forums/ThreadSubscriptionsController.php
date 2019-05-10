fe4504b3bdc452cba4eb24f9392e4907a1977bfb
Meg Stepp <mcstepp@users.noreply.github.com>
2019-05-10 16:03:45 -0400

    manage subscriptions

--- app/Http/Controllers/Forums/ThreadSubscriptionsController.php
+++ app/Http/Controllers/Forums/ThreadSubscriptionsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Forums;
 
 use App\Models\Forum\Thread;
+use App\Models\Subscriptions\ThreadSubscription;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -16,7 +17,12 @@ class ThreadSubscriptionsController extends Controller
     public function index()
     {
         $user = auth()->user();
-        $subscriptions = $user->subscriptions();
+        $sub_thread_ids = ThreadSubscription::where('user_id',$user->id)->pluck('thread_id');
+
+        $subscriptions = Thread::with(['lastPost.author','firstPost.author'])
+            ->whereIn('id', $sub_thread_ids)
+            ->orderBy('updated_at','desc')
+            ->get();
 
         return view('user.manage.subscriptions', [
             'user' => $user,
--- resources/views/user/manage/subscriptions.blade.php
+++ resources/views/user/manage/subscriptions.blade.php
@@ -19,7 +19,37 @@
             <div class="col">
                 <h1 class="m-fancy-header neon-default">Manage Subscriptions</h1>
 
-                <p>Coming soon</p>
+                <div class="card m-card bg-industrial">
+                    <div class="card-body">
+                        <table class="table table-responsive-sm">
+                            <thead>
+                            <tr class="bg-primary text-uppercase m-fancy-title">
+                                <th>Topic Title</th>
+                                <th>Last Post Date</th>
+                                <th>Last Post By</th>
+                                <th>Started By</th>
+                                <th>Unsubscribe</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @forelse($subscriptions as $subscription)
+                                <tr>
+                                    <td><a href="{{ $subscription->path }}">{{ $subscription->title }}</a></td>
+                                    <td>{{ $subscription->lastPost->updated_at->format('F j, Y') }}</td>
+                                    <td><a href="{{ $subscription->lastPost->author->path }}">{{ $subscription->lastPost->author->username }}</a></td>
+                                    <td><a href="{{ $subscription->firstPost->author->path }}">{{ $subscription->firstPost->author->username }}</a></td>
+                                    <td><subscribe-button is-subscribed="true" path="{{ $subscription->path }}"></subscribe-button></td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td>None to show.</td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
