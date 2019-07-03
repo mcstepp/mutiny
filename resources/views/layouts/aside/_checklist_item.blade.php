db13acd08acab5bde17cbcc7a6d9471225c119b1
Meg Stepp <mcstepp@users.noreply.github.com>
2019-07-03 19:57:52 -0400

    update checklist poc

--- resources/views/layouts/aside/_checklist_item.blade.php
+++ resources/views/layouts/aside/_checklist_item.blade.php
@@ -1,9 +1,9 @@
-<div class="card m-card rounded m-text-shadow bg-industrial-dark m-3" style="width: 85%;">
-    <div class="card-body bg-primary clearfix">
+<div class="card m-card m-task rounded m-text-shadow bg-industrial-primary m-3" style="width: 85%;">
+    <div class="card-body clearfix">
         <h6 class="m-fancy-title text-uppercase neon-default">
             Task Category
         </h6>
-        <div class="tab-content border-0 bg-primary text-white h-auto">
+        <div class="tab-content border-0 text-white h-auto bg-transparent">
             <div class="tab-pane p-1 active" id="task-summary-{{ $id }}" role="tabpanel">
                 <p class="small">Aenean sit amet aliquet risus. Curabitur vel auctor ipsum. Vestibulum at accumsan ligula, et accumsan diam. Aliquam id est sit amet ligula molestie pellentesque. Ut dignissim, odio et mattis maximus, dui dui convallis odio, vel aliquam lectus dolor vitae velit.</p>
             </div>
@@ -21,16 +21,16 @@
             </div>
         </div>
 
-        <footer>
-            <ul class="nav nav-tabs border-0" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" data-toggle="tab" href="#task-summary-{{ $id }}" role="tab"><i class="fas fa-comment-alt"></i></a>
+        <footer class="bg-primary">
+            <ul class="nav border-0 justify-content-around nav-fill" role="tablist">
+                <li class="nav-item text-center">
+                    <a class="nav-link text-white active" data-toggle="tab" href="#task-summary-{{ $id }}" role="tab"><i class="fas fa-comment-alt"></i></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#bonus-{{ $id }}" role="tab"><i class="fas fa-list"></i><small class="p-1">1/5</small></a>
+                <li class="nav-item text-center">
+                    <a class="nav-link text-white" data-toggle="tab" href="#bonus-{{ $id }}" role="tab"><i class="fas fa-list"></i><small class="p-1">1/5</small></a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" data-toggle="tab" href="#url-{{ $id }}" role="tab"><i class="fas fa-link"></i></a>
+                <li class="nav-item text-center">
+                    <a class="nav-link text-white" data-toggle="tab" href="#url-{{ $id }}" role="tab"><i class="fas fa-link"></i></a>
                 </li>
             </ul>
         </footer>
